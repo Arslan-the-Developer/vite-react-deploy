35d9362b99ede1f9a9f343b03f49ffa31de34bf6
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-03-17 14:40:57 +0500

    Added Home Screen

---
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+function HomeScreen() {
+
+  return (
+
+    <section>
+        <h2>Home Screen</h2>
+        <h2>Welcome To Website</h2>
+    </section>
+
+  )
+
+}
+
+
+export default HomeScreen
