d5f9f91d3ffb9b18bb783f0e1045c55500c6cfa6
Arslan-the-Developer <callmearslanahmed@gmail.com>
2025-04-16 21:33:28 +0500

    Added Spring Animation Text

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar'
-import BlurText from '../../Components/BlurText/BlurText'
+import RotatingText from '../../Components/RotatingText/RotatingText'
 
 function HomeScreen() {
 
@@ -15,13 +15,18 @@ function HomeScreen() {
             <h2 className='text-3xl mt-6'>Everything Is Working Correctly</h2>
 
             <span className=''>
-            <BlurText
-              text="Arslan Ahmed"
-              delay={150}
-              animateBy="words"
-              direction="top"
-              className="text-2xl mb-8"
-            />
+            <RotatingText
+                texts={['Programmer', 'Full Stack Developer', 'Freelancer', 'API Expert']}
+                mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                staggerFrom={"first"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
             </span>
 
         </div>
